94d963c78a26caae814fcdc6b53fe594c9280139
maxywb <beckett.max@gmail.com>
2016-02-23 07:58:17 -0500

    remove install_deps script and add untracked python util file.

---
+++ python/util.py
@@ -0,0 +1,23 @@
+
+
+
+def split_privmsg(line):
+    words = line.split()
+
+    who = words[0]
+    what = words[1]
+    where = words[2]
+    message = words[3:]
+
+    if len(message) <= 0:
+        return who, what, where, message
+
+    if message[0].startswith(":"):
+        message[0] = message[0][1:]
+
+    if who.startswith(":"):
+        who = who[1:]
+        
+    who = who.split("!")
+
+    return who, what, where, message
